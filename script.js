457215e9625db5e63b6f9ee56e9c560f5d0dea87
itsguysbosthostbot24 <77636716+itsguysbosthostbot24@users.noreply.github.com>
2024-02-23 21:27:39 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
-document.addEventListener("click", function() {
-    window.location.href = "https://www.youtube.com/watch?v=dQw4w9WgXcQ";
-});
+setTimeout(function() {
+    alert("Jónapot, ez a kód lopott ugy ahogy van (Védelem automatikusan bekapcsolva 2024.03.xx-én)");
+}, 2000);
+
+window.onload = function() {
+    var celUrl = "https://www.pelda.hu/celoldal";
+    var linkElem = document.getElementById("autoHrefLink");
+    linkElem.href = celUrl;
+}, 10000;
+
