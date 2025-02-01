3f774859b01208df59fd33e688578a86a1a9bd87
itsguysbosthostbot24 <77636716+itsguysbosthostbot24@users.noreply.github.com>
2025-02-01 02:33:56 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,3 @@
-setTimeout(function() {
-    alert("Jónapot, ez a kód lopott ugy ahogy van (Védelem automatikusan bekapcsolva 2024.03.xx-én)");
-}, 2000);
-
-window.onload = function() {
-    var celUrl = "https://www.youtube.com/watch?v=dQw4w9WgXcQ";
-    var linkElem = document.getElementById("autoHrefLink");
-    linkElem.href = celUrl;
-}, 10000;
-
+setTimeout(() => {
+    alert("alma");
+}, 5000); 
